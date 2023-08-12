177885e43c0ece308e081766e02dfd51de4ff0f5
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-08-12 16:04:43 -0500

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/pages/HomeScreen";
+import ProfileScreen from "./src/pages/ProfileScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+}
