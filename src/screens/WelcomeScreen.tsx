713b9743e64409b44b644284f8601944a22a7035
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-08-13 11:25:38 -0500

    [New]: Wecome Screen UI

---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,46 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import CustomInput from "../components/CustomInput/CustomInput";
+import tw from "../lib/tailwind";
+import CustomButton from "../components/CustomButton/CustomButton";
+import LottieView from "lottie-react-native";
+
+export default function SignInScreen() {
+  const onSignInPressed = () => {
+    console.warn("Sign In");
+  };
+
+  const onSignUpPressed = () => {
+    console.warn("Sign Up");
+  };
+
+  return (
+    <SafeAreaView style={tw`h-full w-full bg-background`}>
+      {/* Top Circle */}
+      <View
+        style={tw`w-[400px] h-[400px] absolute rounded-full bg-secondaryGray/20 -top-10 -left-30`}
+      />
+      <View style={tw`w-80 h-full items-center m-auto`}>
+        <LottieView
+          source={require("../assets/Avatar.json")}
+          style={tw`h-full w-full absolute`}
+          autoPlay
+          loop
+        />
+        <View style={tw`z-10 h-full justify-end bottom-40 gap-5`}>
+          <Text
+            style={tw`font-extrabold text-center text-base text-white underline tracking-widest`}
+          >
+            WELCOME
+          </Text>
+          <CustomButton text={"Sign In"} onPress={onSignInPressed} />
+          <CustomButton text={"Sign Up"} onPress={onSignUpPressed} />
+          {/* Bottom Circle */}
+        </View>
+        <View
+          style={tw`w-[200px] h-[200px] absolute rounded-full bg-primaryWhite -bottom-10 -right-20`}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
