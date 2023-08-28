cf1db131ca1ab15854438f03e02cd70ea7a6e9f6
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-08-28 14:19:46 -0500

    [Update]: Feed layout

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,20 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { HomeStackNavigationProp } from "../types/screens.definition";
 import { auth } from "../firebase/firebase";
 import tw from "../lib/tailwind";
 import * as SecureStore from "expo-secure-store";
 import { useAppDispatch, useAppSelector } from "../redux/hooks/hooks";
 import { setToken } from "../redux/slices/userSlice";
+import { User } from "../lib/data";
+import MiniProfile from "../components/MiniProfile";
+import Post from "../components/Post";
 
 export default function HomeScreen({
   navigation,
@@ -14,6 +23,22 @@ export default function HomeScreen({
 }) {
   const user = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
+  const [users, setUsers] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const users = await fetch("https://random-data-api.com/api/v2/users", {
+          method: "GET",
+        });
+        const data = await users.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
 
   const handleSignOut = () => {
     auth
@@ -28,15 +53,13 @@ export default function HomeScreen({
 
   return (
     <SafeAreaView style={tw`bg-background h-full`}>
-      <View
-        style={tw`w-80 bg-primaryWhite justify-center items-center mx-auto`}
-      >
-        <TouchableOpacity
-          onPress={handleSignOut}
-          style={tw`bg-background rounded-md items-center p-3`}
-        >
-          <Text style={tw`text-white`}>Sign Out</Text>
-        </TouchableOpacity>
+      <View style={tw`w-80 h-full mx-auto gap-15`}>
+        <View style={tw`flex-1 bg-secondaryGray/20 rounded-xl`}>
+          <MiniProfile user={users} />
+        </View>
+        <View style={tw`flex-2 bg-secondaryGray/20 rounded-xl`}>
+          <Post users={users} />
+        </View>
       </View>
     </SafeAreaView>
   );
