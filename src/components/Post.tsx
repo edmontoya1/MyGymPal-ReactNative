97c5c0cfee03512f1ae14d307c6c2800fa2b484e
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-09-04 12:48:50 -0500

    [Update]: changed to styled components

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,16 +1,33 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Dimensions,
+} from "react-native";
 import React from "react";
-import tw from "../lib/tailwind";
 import { User } from "../lib/data";
 
 export default function Post(prop: { users: User | null }) {
   return (
-    <View style={tw`h-full bg-primaryWhite`}>
-      <Text>{prop.users?.username}</Text>
-      <Image
-        source={require("../assets/icon.png")}
-        style={{ width: "auto", height: 350 }}
-      />
+    <View style={styles.container}>
+      <Text>{prop.users?.first_name}</Text>
+      <Image source={require("../assets/icon.png")} style={styles.img} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    height: Dimensions.get("window").height - 75,
+    borderWidth: 2,
+    borderRadius: 15,
+  },
+  img: {
+    height: 50,
+    width: 50,
+  },
+});
