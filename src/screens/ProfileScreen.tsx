282ae84c8ac4fd90dae52be59d87a1ded44639e7
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-09-24 16:35:15 -0500

    temp

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
 import React from "react";
+import { auth } from "../firebase/firebase";
 
 export default function ProfileScreen() {
+  const handleLogOut = () => {
+    auth.signOut();
+  };
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>ProfileScreen</Text>
-    </View>
+      <TouchableOpacity>
+        <Text>Log out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
