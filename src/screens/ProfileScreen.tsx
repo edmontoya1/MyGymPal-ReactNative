a5820552a90c325497844d302130eb971e32bc2e
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-09-24 23:13:04 -0500

    [Update]: signout functionality

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,16 +1,25 @@
 import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
 import React from "react";
 import { auth } from "../firebase/firebase";
+import { useAppDispatch } from "../redux/hooks/hooks";
+import { setToken } from "../redux/slices/userSlice";
+import * as SecureStore from "expo-secure-store";
 
 export default function ProfileScreen() {
+  const dispatch = useAppDispatch();
+
   const handleLogOut = () => {
-    auth.signOut();
+    auth.signOut().then(async () => {
+      dispatch(setToken(null));
+      await SecureStore.deleteItemAsync("userToken");
+      alert("signed out");
+    });
   };
 
   return (
     <SafeAreaView>
       <Text>ProfileScreen</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleLogOut}>
         <Text>Log out</Text>
       </TouchableOpacity>
     </SafeAreaView>
