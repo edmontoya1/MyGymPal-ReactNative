d3241dec98a5cbd81c4bafa7539209ced08f58ab
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-10-05 11:44:22 -0500

    [New]: Added createAsyncThunk for fetching

--- src/redux/slices/postSlice.ts
+++ src/redux/slices/postSlice.ts
@@ -1,17 +1,66 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { IPost } from "../../types/post.interface";
 import { RootState } from "../store";
-
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+// onSnapshot(
+//   query(collection(db, "posts"), orderBy("timestamp", "desc")),
+//   (snapshot) => {
+//     const posts = snapshot.docs;
+//     const formattedPosts: IPost[] = posts.map((doc) => {
+//       const { token, username, profileImg, timestamp, image } = doc.data(); // Assuming data() returns the post data
+//       const formattedTimeStamp = timestamp.toDate().toISOString();
+//       return {
+//         token,
+//         username,
+//         profileImg,
+//         timestamp: formattedTimeStamp,
+//         image,
+//       };
+//     });
+//     dispatch(setPosts(formattedPosts));
+//   }
+// );
+export const fetchAllPosts = createAsyncThunk(
+  "users/fetchAllPosts",
+  async () => {
+    try {
+      const postsCollection = collection(db, "posts");
+      const postQuery = query(postsCollection, orderBy("timestamp", "desc"));
+      const postsSnapshot = await getDocs(postQuery);
+      const posts = postsSnapshot.docs;
+      const formattedPosts: IPost[] = posts.map((doc) => {
+        const { token, username, profileImg, timestamp, image } = doc.data(); // Assuming data() returns the post data
+        const formattedTimeStamp = timestamp.toDate().toISOString();
+        return {
+          token,
+          username,
+          profileImg,
+          timestamp: formattedTimeStamp,
+          image,
+        };
+      });
+      return formattedPosts;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+);
 interface PostState {
   currentPost: IPost | null;
   currentIndex: number;
   posts: IPost[] | null;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: null;
 }
 
 const initialState: PostState = {
   currentPost: null,
   currentIndex: 0,
   posts: null,
+  status: "idle",
+  error: null,
 };
 
 export const postSlice = createSlice({
@@ -25,11 +74,27 @@ export const postSlice = createSlice({
       state.posts = action.payload;
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAllPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(fetchAllPosts.rejected, (state, action) => {
+        state.status = "failed";
+      });
+  },
 });
 
 export const { setCurrentPost, setPosts } = postSlice.actions;
 
 export const selectPosts = (state: RootState) => state.post.posts;
+export const getPostsStatus = (state: RootState) => state.post.status;
+export const getPostsError = (state: RootState) => state.post.error;
+
 export const selectCurrentPost = (state: RootState) => state.post.currentPost;
 export const selectCurrentIndex = (state: RootState) => state.post.currentIndex;
 
--- src/redux/slices/usersSlice.ts
+++ src/redux/slices/usersSlice.ts
@@ -1,15 +1,34 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { User } from "../../lib/data";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
+export const fetchAllUsers = createAsyncThunk(
+  "users/fetchAllUsers",
+  async () => {
+    try {
+      const usersCollection = collection(db, "users");
+      const usersSnapshot = await getDocs(usersCollection);
+      return usersSnapshot.docs.map((user) => user.data() as User);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+);
 interface UsersState {
-  loadedUsers: User[];
+  loadedUsers: User[] | null;
   currentIndex: number;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: null;
 }
 
 const initialState: UsersState = {
-  loadedUsers: [],
+  loadedUsers: null,
   currentIndex: 0,
+  status: "idle",
+  error: null,
 };
 
 export const usersSlice = createSlice({
@@ -23,10 +42,25 @@ export const usersSlice = createSlice({
       state.currentIndex = action.payload;
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAllUsers.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllUsers.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.loadedUsers = action.payload;
+      })
+      .addCase(fetchAllUsers.rejected, (state, action) => {
+        state.status = "failed";
+      });
+  },
 });
 
 export const { setLoadedUsers, setCurrentIndexSlice } = usersSlice.actions;
 
 export const selectLoadedUsers = (state: RootState) => state.users.loadedUsers;
+export const getLoadedUsersStatus = (state: RootState) => state.users.status;
+export const getLoadedUsersError = (state: RootState) => state.users.error;
 
 export default usersSlice.reducer;
