4c558a2d95d4831d7eacddda7c004033d683805d
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-10-05 16:45:35 -0500

    [New]: added user interface

---
+++ src/types/user.interface.ts
@@ -0,0 +1,13 @@
+export type IUser = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  followers_count: number;
+  following_count: number;
+  photoUrl: string;
+  pr_squat: number;
+  pr_bench: number;
+  pr_deadlift: number;
+  username: string;
+};
