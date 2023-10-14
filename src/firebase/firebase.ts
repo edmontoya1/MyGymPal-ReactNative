5b33424b1bb934e404911d7dc107439573e676ce
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-10-13 23:48:02 -0500

    [Update]: Added function to upload image to firebase storage

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,7 +3,13 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  listAll,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -28,4 +34,51 @@ const auth = initializeAuth(app, {
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+const listFiles = async () => {
+  const storage = getStorage();
+
+  // Create a reference under which you want to list
+  const listRef = ref(storage, "images");
+
+  // Find all the prefixes and items.
+  const listResp = await listAll(listRef);
+  return listResp.items;
+};
+
+const uploadToFirebase = async (
+  uri: string,
+  name: string | undefined,
+  onProgress: any
+) => {
+  const fetchResponse = await fetch(uri);
+  const theBlob = await fetchResponse.blob();
+
+  const imageRef = ref(getStorage(), `images/${name}`);
+
+  const uploadTask = uploadBytesResumable(imageRef, theBlob);
+
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        onProgress && onProgress(progress);
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+        reject(error);
+      },
+      async () => {
+        const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+        resolve({
+          downloadUrl,
+          metadata: uploadTask.snapshot.metadata,
+        });
+      }
+    );
+  });
+};
+
+export { auth, db, storage, uploadToFirebase, listFiles };
