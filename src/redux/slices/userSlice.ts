bc6d186b971d06b0adda18077b0e4791233ab912
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-10-22 17:39:21 -0500

    [Update]: Async function and set state

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,34 +1,47 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { IUser } from "../../types/user.interface";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import * as SecureStore from "expo-secure-store";
 
+export const fetchUserById = createAsyncThunk(
+  "user/fetchUserById",
+  async (id: string) => {
+    try {
+      const userRef = collection(db, "users");
+      // should only recieve one doc
+      const userQuery = query(userRef, where("userUID", "==", id));
+      return await getDocs(userQuery).then(async (snapshot) => {
+        await SecureStore.setItemAsync("userDocId", snapshot.docs[0].id);
+        return snapshot.docs[0].data() as IUser;
+      });
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+);
 interface UserState {
   token: string | null;
-  id: string | null;
-  email: string | null;
-  first_name: string | null;
-  last_name: string | null;
-  followers_count: number | null;
-  following_count: number | null;
-  photoUrl: string | null;
-  pr_squat: number | null;
-  pr_bench: number | null;
-  pr_deadlift: number | null;
-  username: string | null;
+  user: IUser | null;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: null;
 }
 
 const initialState: UserState = {
   token: null,
-  id: null,
-  email: null,
-  first_name: null,
-  last_name: null,
-  followers_count: null,
-  following_count: null,
-  photoUrl: null,
-  pr_squat: null,
-  pr_bench: null,
-  pr_deadlift: null,
-  username: null,
+  user: null,
+  status: "idle",
+  error: null,
 };
 
 export const userSlice = createSlice({
@@ -38,18 +51,40 @@ export const userSlice = createSlice({
     setToken: (state, action: PayloadAction<string | null>) => {
       state.token = action.payload;
     },
+    setUser: (state, action: PayloadAction<IUser>) => {
+      state.user = action.payload;
+    },
     setFirstName: (state, action: PayloadAction<string>) => {
-      state.first_name = action.payload;
+      if (state.user?.firstName) {
+        state.user.firstName = action.payload;
+      }
     },
-    setId: (state, action: PayloadAction<string>) => {
-      state.id = action.payload;
+    setUserUID: (state, action: PayloadAction<string>) => {
+      if (state.user?.userUID) {
+        state.user.userUID = action.payload;
+      }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUserById.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchUserById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload;
+      })
+      .addCase(fetchUserById.rejected, (state, action) => {
+        state.status = "failed";
+      });
   },
 });
 
-export const { setFirstName, setToken, setId } = userSlice.actions;
+export const { setFirstName, setToken, setUserUID, setUser } =
+  userSlice.actions;
 
-export const selectFirstName = (state: RootState) => state.user.first_name;
+export const selectFirstName = (state: RootState) => state.user.user?.firstName;
 export const selectToken = (state: RootState) => state.user.token;
+export const selectUser = (state: RootState) => state.user.user;
 
 export default userSlice.reducer;
