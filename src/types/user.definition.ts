0767cfffd1cce7069fe69d650f4b8c9547daa88e
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-11-05 10:39:53 -0600

    updated definitions

--- src/types/user.definition.ts
+++ src/types/user.definition.ts
@@ -5,11 +5,20 @@ export type IUser = {
 	lastName: string;
 	followers_count: number;
 	following_count: number;
-	photoUri: string;
 	pr_squat: number;
 	pr_bench: number;
 	pr_deadlift: number;
 	username: string;
 	password: string | undefined;
 	token: string | undefined;
+};
+
+export type UserSignUp = {
+	email: string;
+	password: string;
+	imageUri: string;
+	imageDownloadUrl: string;
+	firstName: string;
+	lastName: string;
+	userName: string;
 };
