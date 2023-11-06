da31adb52750c3c80976343a0684599ffadc15ad
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-11-06 17:35:26 -0600

    temp profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,13 +1,29 @@
 import * as SecureStore from "expo-secure-store";
-import React from "react";
-import { Text, TouchableOpacity, SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { Text, TouchableOpacity, SafeAreaView, ScrollView, Image, StyleSheet } from "react-native";
 
-import { auth } from "../firebase/firebase";
-import { useAppDispatch } from "../redux/hooks/hooks";
-import { setToken } from "../redux/slices/userSlice";
+import { auth, fetchUserByUID } from "../firebase/firebase";
+import { useAppDispatch, useAppSelector } from "../redux/hooks/hooks";
+import { setToken, setUser } from "../redux/slices/userSlice";
+import { IUser } from "../types/user.definition";
 
 export default function ProfileScreen() {
 	const dispatch = useAppDispatch();
+	const currentUser = useAppSelector((state) => state.user.user);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			// If authenticated, fetch the user
+			if (auth.currentUser) {
+				const user = await fetchUserByUID(auth.currentUser?.uid);
+
+				if (user) {
+					dispatch(setUser(user.docs[0].data() as IUser));
+				}
+			}
+		};
+		fetchUser();
+	}, []);
 
 	const handleLogOut = () => {
 		auth.signOut().then(async () => {
@@ -17,13 +33,26 @@ export default function ProfileScreen() {
 			alert("signed out");
 		});
 	};
-
+	console.log(currentUser?.profileImageDownloadUrl);
 	return (
 		<SafeAreaView>
+			<Image source={{ uri: currentUser?.profileImageDownloadUrl }} style={styles.profileImage} />
 			<Text>ProfileScreen</Text>
 			<TouchableOpacity onPress={handleLogOut}>
 				<Text>Log out</Text>
 			</TouchableOpacity>
+			<ScrollView>
+				<Text>{JSON.stringify(auth.currentUser, null, 2)}</Text>
+				<Text>---</Text>
+				<Text>{JSON.stringify(currentUser, null, 2)}</Text>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	profileImage: {
+		height: 100,
+		width: 100
+	}
+});
